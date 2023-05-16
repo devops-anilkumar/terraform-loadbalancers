2ddf7ea34e3b0b81f2605aefda1bdaa53c9e6fcd
unknown <anilkumarravuri510@gmail.com>
2023-05-17 00:07:05 +0530

    auto push

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "alb-public" {
   source                    = "./vendor/modules/alb" # terrafile is going to download code from the respective branch of the repo and keep it local
    ALB_NAME                 = "robot-public-alb"
-   ENV                      = var.ENV
    INTERNAL                 = false
+    ENV                     = var.ENV
  
   # VPC_CIDR                  = var.VPC_CIDR
 
